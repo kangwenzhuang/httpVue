952e7d012d8d014eb9152ea4988f8f2205663367
kangwenzhuang <1519752991@qq.com>
2019-05-13 10:56:44 +0800

    zengjia

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-button :plain="true" @click="open">打开消息提示</el-button>
+    <el-button :plain="true" @click="openVn">VNode</el-button>
+    <el-button type="primary" icon="el-icon-search" @click="ui">搜索</el-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  methods: {
+    open() {
+      this.$message("这是一条消息提示");
+    },
+    ui() {
+      alert("wojiao康文壮将");
+    },
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    openVn() {
+      const h = this.$createElement;
+      this.$message({
+        message: h("p", null, [
+          h("span", null, "内容可以是 "),
+          h("i", { style: "color: teal" }, "VNode")
+        ])
+      });
+    }
+  }
+};
+</script>
