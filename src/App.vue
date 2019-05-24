786a05373cbc0e52a5f79b29592470abafb0675d
kangwenzhuang <1519752991@qq.com>
2019-05-24 15:03:08 +0800

    http请求

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div>
     <el-button :plain="true" @click="open">打开消息提示</el-button>
     <el-button :plain="true" @click="openVn">VNode</el-button>
-    <el-button type="primary" icon="el-icon-search" @click="sayHello">搜索</el-button>
+    <el-button type="primary" icon="el-icon-search" @click="addUser">添加用户</el-button>
+    <el-button type="primary" icon="el-icon-search" @click="userList">用户列表</el-button>
     <el-button>hello</el-button>
   </div>
 </template>
@@ -26,12 +27,30 @@ export default {
         ])
       });
     },
-    async sayHello() {
-      console.log("udfhiefhfedgfie");
+    async addUser() {
       await this.$http
-        .post("http://172.17.173.150:8080/signin/test", {})
+        .post("http://kangkangin.asia:8080/signin/addUser", {
+          'xuehao':'123456784',
+          'name':'kangwenzhuang11',
+          'phone':'17805973818',
+          'mail':'1519752991@qq.com',
+          'password':'123456',
+          'shenfen':2,
+          'sex':'男',
+          'motto':'福大数计学院2号楼304',
+        })
         .then(res => {
-          console.log("我真的很难受");
+          console.log("添加信息："+res['body'].resultMessage);//因为存在重复添加所以会返回信息，学号和身份同时重复视为添加失败;
+        });
+    },
+    async userList() {
+      await this.$http
+        .post("http://172.17.173.150:8080/signin/userList", {
+          'shenfen':2,
+        })
+        .then(res => {
+          console.log("用户列表"+res['body'].data);//你需要在前端定义一个数组接收,此时，打印结果是object，想看结果的话JSON.stringify(res['body'].data)，所以要定义一个数组，例如前端user[] 接收，你百度解决;
+          console.log("用户列表"+JSON.stringify(res['body'].data));
         });
     }
   }
